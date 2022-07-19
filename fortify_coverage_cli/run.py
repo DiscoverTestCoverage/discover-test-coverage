7c494fed753969dad9b6acd95e84ad2deb82270d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-19 14:05:02 -0400

    feat: Add functions to run that support executing the test suite.

--- fortify_coverage_cli/run.py
+++ fortify_coverage_cli/run.py
@@ -7,6 +7,8 @@ import subprocess
 
 from enum import Enum
 from pathlib import Path
+from shutil import copytree
+from shutil import rmtree
 
 
 class TestRunCommand(str, Enum):
@@ -16,14 +18,36 @@ class TestRunCommand(str, Enum):
     TEST_FAIL_FAST = "poetry run pytest -x"
 
 
+def copytree_overwrite(from_path, to_path):
+    if os.path.exists(to_path):
+        rmtree(to_path)
+    copytree(from_path, to_path)
+
+
 def run_test_suite(
-    project_directory: Path, program_directory: Path, test_run_command: str
+    project_directory: Path,
+    program_directory: Path,
+    test_directory: Path,
+    test_run_command: str,
 ) -> None:
     """Run the test suite with a provided command."""
     output.logger.debug(f"Change into the project directory: {project_directory}")
     initial_current_working_directory = Path.cwd()
     os.chdir(project_directory)
     output.print_test_start()
+    test_directory = Path(project_directory / test_directory)
+    test_directory_backup = Path(
+        project_directory / ("." + test_directory.name + "-backup")
+    )
+    test_directory_instrumented = Path(project_directory / ("." + test_directory.name))
+    copytree_overwrite(test_directory, test_directory_backup)
+    rmtree(test_directory)
+    copytree_overwrite(test_directory_instrumented, test_directory)
     subprocess.run(test_run_command, shell=True)
     output.print_test_finish()
+    rmtree(test_directory)
+    # test_directory.rmdir()
+    copytree_overwrite(test_directory_backup, test_directory)
+    # test_directory_backup.rmdir()
+    rmtree(test_directory_backup)
     os.chdir(initial_current_working_directory)
