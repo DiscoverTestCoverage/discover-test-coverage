6e5ed830276ebf3270e849a957053cf805159c06
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-19 12:34:32 -0400

    refactor: Delete not-needed visit methods in TestFixtureTransformer in testfixtures.

--- fortify_coverage_cli/transformers/testfixtures.py
+++ fortify_coverage_cli/transformers/testfixtures.py
@@ -20,33 +20,7 @@ class TestFixtureTransformer(cst.CSTTransformer):
     """Transform program source code to collect information about test execution."""
 
     def __init__(self):
-        # stack for storing the canonical name of the current function
-        self.stack: List[Tuple[str, ...]] = []
-
-    def visit_ClassDef(self, node: cst.ClassDef) -> Optional[bool]:
-        self.stack.append(node.name.value)
-
-    def leave_ClassDef(
-        self, original_node: cst.ClassDef, updated_node: cst.ClassDef
-    ) -> cst.CSTNode:
-        self.stack.pop()
-        return updated_node
-
-    def visit_FunctionDef(self, node: cst.FunctionDef) -> Optional[bool]:
-        self.stack.append(node.name.value)
-        return False
-
-    def leave_FunctionDef(
-        self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef
-    ) -> cst.CSTNode:
-        key = tuple(self.stack)
-        self.stack.pop()
-        output.logger.debug(
-            f"current function definition node's name: {original_node.name}"
-        )
-        output.logger.debug(f"contents of the stack after the pop() call: {self.stack}")
-        # return updated_node.with_changes(decorators=[cst.Decorator(cst.Name("sample"))])
-        return updated_node
+        self.name = "TestFixtureTransformer"
 
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
