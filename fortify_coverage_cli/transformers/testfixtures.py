6c6c76c5020c5c482ea25146e0f61cd4e369195f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-19 13:25:27 -0400

    feat: Improve leave_Module function in TestFixtureTransformer of testfixtures to use new approach.

--- fortify_coverage_cli/transformers/testfixtures.py
+++ fortify_coverage_cli/transformers/testfixtures.py
@@ -30,35 +30,50 @@ class TestFixtureTransformer(cst.CSTTransformer):
     """Transform test suite source code to collect information about test execution."""
 
     def __init__(self):
+        """Construct a TestFixtureTransformer and give it a name."""
         self.name = "TestFixtureTransformer"
 
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
     ) -> cst.CSTNode:
-        output.logger.debug("start --->")
-        output.logger.debug(f"current module node's header: {original_node.header}")
+        """Instrument the starting source code in the module to add test fixture import."""
         output.logger.debug(
-            f"current module node's body first: {original_node.body[0]}"
+            f"Modifying source code body with starting length: {len(updated_node.body)}"
         )
-        output.logger.debug("---> end")
-        import_statement_type = codegenerator.InstrumentationTypeSourceCode.TEST_SESSION_DOCSTRONG
-        instrumentation_type_generator = codegenerator.InstrumentedSourceCodeGenerator(import_statement_type)
-        import_statement = instrumentation_type_generator.generate()
         # determine whether or not the module has a docstring
         module_has_docstring = detect_module_docstring(original_node)
+        # the module has a docstring and thus the import statement should
+        # be inserted after the docstring and before any other imports
         if module_has_docstring:
+            # generate the correct type of import statement
+            import_statement_type = (
+                codegenerator.InstrumentationTypeSourceCode.TEST_SESSION_DOCSTRONG
+            )
+            instrumentation_type_generator = codegenerator.InstrumentedSourceCodeGenerator(
+                import_statement_type
+            )
+            import_statement = instrumentation_type_generator.generate()
+            # insert the import statement between the docstring and the rest of the code
             body_modified = (
                 *updated_node.body[0:1],
                 import_statement,
                 *updated_node.body[1:],
             )
-            output.logger.debug(f"length of body modified: {len(body_modified)}")
             updated_node = updated_node.with_changes(body=body_modified)
+        # the module does not have a docstring and thus the import statement
+        # should be imported as the first line inside of the module's body
         else:
+            import_statement_type = (
+                codegenerator.InstrumentationTypeSourceCode.TEST_SESSION_DOCSTRONG
+            )
+            instrumentation_type_generator = codegenerator.InstrumentedSourceCodeGenerator(
+                import_statement_type
+            )
+            import_statement = instrumentation_type_generator.generate()
             body_modified = (
                 import_statement,
                 *updated_node.body,
             )
-            output.logger.debug(f"length of body modified: {len(body_modified)}")
+            output.logger.debug(f"Modified source code body to have length: {len(body_modified)}")
             updated_node = updated_node.with_changes(body=body_modified)
         return updated_node
