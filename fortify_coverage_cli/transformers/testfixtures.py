1270d1df4efc6810f521b15195ff058ac9e31b80
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-19 12:32:02 -0400

    feat: Add rough, but working, implementation in testfixtures.

--- fortify_coverage_cli/transformers/testfixtures.py
+++ fortify_coverage_cli/transformers/testfixtures.py
@@ -3,11 +3,14 @@
 from fortify_coverage_cli import output
 from fortify_coverage_cli import transform
 
+from datetime import datetime
+
 from typing import List
 from typing import Optional
 from typing import Tuple
 
 import libcst as cst
+from libcst import SimpleStatementLine, Expr, SimpleString
 
 
 class TestFixtureTransformer(cst.CSTTransformer):
@@ -39,23 +42,59 @@ class TestFixtureTransformer(cst.CSTTransformer):
             f"current function definition node's name: {original_node.name}"
         )
         output.logger.debug(f"contents of the stack after the pop() call: {self.stack}")
-        return updated_node.with_changes(decorators=[cst.Decorator(cst.Name("sample"))])
+        # return updated_node.with_changes(decorators=[cst.Decorator(cst.Name("sample"))])
+        return updated_node
 
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
     ) -> cst.CSTNode:
         output.logger.debug("start --->")
         output.logger.debug(f"current module node's header: {original_node.header}")
-        output.logger.debug("---> end")
-        # TODO: Add in correct location and add correct import to a
-        # module that exists inside of the fortify-coverage package
-        updated_node = updated_node.with_changes(
-            header=(
-                cst.parse_statement(
-                    "from fortify import sample # leave_Module",
-                    config=transform.source_tree_configuration,
-                ),
-                *updated_node.header,
-            )
+        output.logger.debug(
+            f"current module node's body first: {original_node.body[0]}"
         )
+        output.logger.debug(
+            f"length of current module node's body: {len(original_node.body)}"
+        )
+        output.logger.debug("---> end")
+        output.logger.debug(
+            f"Does the module have a docstring? {self.has_module_docstring(original_node)}"
+        )
+        output.logger.debug(f"Node header: {updated_node.header}")
+        output.logger.debug(f"Node body: {updated_node.body}")
+        multiple_line_import_statement_str = (
+            f"\n# fortify-coverage instrumentation generated on {datetime.now().strftime('%m/%d/%Y at %H:%M:%S')}\n"
+            "from fortify_coverage.fixture import session_setup_teardown"
+        )
+        import_statement = cst.parse_statement(
+            multiple_line_import_statement_str,
+            config=transform.source_tree_configuration,
+        )
+        body_modified = (
+            *updated_node.body[0:1],
+            import_statement,
+            *updated_node.body[1:],
+        )
+        output.logger.debug(f"length of body modified: {len(body_modified)}")
+        updated_node = updated_node.with_changes(body=body_modified)
+        # updated_node = updated_node.with_changes(
+        #     header=(
+        #         cst.parse_statement(
+        #             "from fortify import sample # leave_Module",
+        #             config=transform.source_tree_configuration,
+        #         ),
+        #         *updated_node.header,
+        #     )
+        # )
         return updated_node
+
+    @staticmethod
+    def has_module_docstring(node):
+        has_module_docstring = False
+        n = node.body[0]
+        if isinstance(n, SimpleStatementLine):
+            n = n.body[0]
+            if isinstance(n, Expr):
+                if len(n.children) == 1 and isinstance(n.children[0], SimpleString):
+                    has_module_docstring = True
+        return has_module_docstring
