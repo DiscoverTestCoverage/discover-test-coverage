e56a9a27c530cd581ac771482e9a855b833776ff
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-26 13:09:22 -0400

    refactor: Change import names and reorder imports in run module.

--- discover/run.py
+++ discover/run.py
@@ -1,15 +1,14 @@
 """Run commands to execute commands of a subject."""
 
-from fortify_coverage_cli import output
-from fortify_coverage_cli import constants
-
 import os
 import subprocess
-
 from enum import Enum
 from pathlib import Path
 from shutil import copytree
 from shutil import rmtree
+
+from discover import constants
+from discover import output
 
 
 class TestRunCommand(str, Enum):
